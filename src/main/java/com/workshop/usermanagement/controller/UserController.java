2bda55ce6f6d6a53687dae91ffe4dd45564dee52
MadalinaKhaji <madalina.khaji10@gmail.com>
2022-02-18 16:19:11 +0200

    implemented user edit, delete and get all users

--- src/main/java/com/workshop/usermanagement/controller/UserController.java
+++ src/main/java/com/workshop/usermanagement/controller/UserController.java
@@ -4,6 +4,8 @@ import com.workshop.usermanagement.dto.UserDto;
 import com.workshop.usermanagement.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,5 +24,20 @@ public class UserController {
     @GetMapping("/{id}")
     public UserDto getUser(@PathVariable Integer id) {
         return userService.getUser(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userService.deleteUser(id);
+    }
+
+    @PatchMapping
+    public UserDto updateUser(@RequestBody UserDto userDto) {
+        return userService.updateUser(userDto);
+    }
+
+    @GetMapping("/all")
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
--- src/main/java/com/workshop/usermanagement/service/UserService.java
+++ src/main/java/com/workshop/usermanagement/service/UserService.java
@@ -7,6 +7,10 @@ import com.workshop.usermanagement.entity.UserEntity;
 import com.workshop.usermanagement.repo.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class UserService {
 
@@ -28,5 +32,28 @@ public class UserService {
 
     public UserDto getUser(Integer userId) {
         return mapper.map(userRepository.findById(userId).get(), UserDto.class);
+    }
+
+    public void deleteUser(Integer userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public UserDto updateUser(UserDto userDto) {
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+
+        UserEntity updatedUser = userRepository.save(userEntity);
+
+        return mapper.map(updatedUser, UserDto.class);
+    }
+
+    public List<UserDto> getAllUsers() {
+        List<UserEntity> userEntityList = new ArrayList<>();
+
+        userRepository.findAll().forEach((userEntity -> userEntityList.add(userEntity)));
+
+        return userEntityList
+                .stream()
+                .map(userEntity -> mapper.map(userEntity, UserDto.class))
+                .collect(Collectors.toList());
     }
 }
